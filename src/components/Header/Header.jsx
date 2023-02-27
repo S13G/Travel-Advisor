8f30aac9e55de2bc31109edbc6c118ed5e902b1e
s13g <ayflix0@gmail.com>
2023-02-27 19:28:38 +0100

    working on the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,1 +1,31 @@
 import React from 'react';
+import {AppBar, Box, InputBase, Toolbar, Typography} from "@mui/material";
+import {Autocomplete} from "@react-google-maps/api";
+import SearchIcon from '@mui/icons-material';
+
+const Header = () => {
+    return (
+        <AppBar position={"static"}>
+            <Toolbar className={classes.toolbar}>
+                <Typography variant={"h5"} className={classes.title}>
+                    Travel Advisor
+                </Typography>
+                <Box display={"flex"}> {/* This is like a div   */}
+                    <Typography variant={"h6"} className={classes.title}>
+                        Explore new places
+                    </Typography>
+                    <Autocomplete>
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon/>
+                            </div>
+                            <InputBase placeholder={"Search....."} classes={{root: classes.InputRoot, input: classes.inputInput}}/>
+                        </div>
+                    </Autocomplete>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default Header;
